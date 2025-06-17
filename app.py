85da124b5e41c924211d56a97a4289dbf6967af5
Aditi Kamble <124804576+aditikamble123@users.noreply.github.com>
2025-06-17 13:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,12 +172,12 @@ if st.button("✨ Get My Personality Type"):
         ])
 
         definition = mbti_df[mbti_df["Type"] == mbti]["Definition"].values[0]
-st.markdown(f"""
-    <div style='background-color: #ffeaea; padding: 20px; border-radius: 15px;'>
-        <h3 style='color: purple;'>Your MBTI Type: {mbti_type} 🌟</h3>
-        <p style='color: black;'>{description}</p>
-    </div>
-""", unsafe_allow_html=True)
+       st.markdown(f"""
+          <div style='background-color: #ffeaea; padding: 20px; border-radius: 15px;'>
+          <h3 style='color: purple;'>Your MBTI Type: {mbti_type} 🌟</h3>
+          <p style='color: black;'>{description}</p>
+         </div>
+         """, unsafe_allow_html=True)
 
 
         pdf_buffer = create_pdf(mbti, definition)
